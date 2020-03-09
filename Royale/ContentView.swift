5f5c4346b0442d3f48ec2e8e385fb056d34e017c
Arnav <arnavdev404@gmail.com>
2020-03-08 20:44:35 -0700

    Initial Commit

---
+++ Royale/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Royale
+//
+//  Created by Arnav Thirunagari on 3/8/20.
+//  Copyright © 2020 Arnav Thirunagari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
